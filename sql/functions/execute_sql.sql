3edee2f29737645084bc52832380373c542640e0
Mark Burggraf <markb@mantisbible.com>
2021-10-02 10:29:28 -0700

    added sql

---
+++ sql/functions/execute_sql.sql
@@ -0,0 +1,22 @@
+CREATE EXTENSION IF NOT EXISTS PLV8;
+DROP FUNCTION execute_sql;
+CREATE OR REPLACE FUNCTION execute_sql(sqlcode TEXT) RETURNS JSON SECURITY DEFINER AS $$
+
+const arr = sqlcode.split(';');
+const results = [];
+
+// this handles "TypeError: Do not know how to serialize a BigInt"
+function toJson(data) {
+  if (data !== undefined) {
+    return JSON.stringify(data, (_, v) => typeof v === 'bigint' ? `${v}#bigint` : v)
+        .replace(/"(-?\d+)#bigint"/g, (_, a) => a);
+  }
+}
+
+for (let i = 0; i < arr.length; i++) {
+  const result = plv8.execute(arr[i]);
+  results.push(toJson(result));
+}
+return results;
+
+$$ language PLV8;
