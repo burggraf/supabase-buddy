752f23b9f7b9b6e56cc59ff636b10c65e649edd2
Mark Burggraf <markb@mantisbible.com>
2021-10-04 19:45:18 -0700

    added monoco for editing database function (work in progress)

--- src/pages/DatabaseFunction.tsx
+++ src/pages/DatabaseFunction.tsx
@@ -11,16 +11,24 @@ import {
 	IonTitle,
 	IonToolbar,
 } from '@ionic/react'
+import Editor from '@monaco-editor/react'
 import { useEffect, useState } from 'react'
 import { useHistory, useParams } from 'react-router'
 import { SupabaseDataService } from '../services/supabase.data.service'
 import './DatabaseFunction.css'
+import { debounce } from 'ts-debounce'
 
 const DatabaseFunction: React.FC = () => {
     const history = useHistory();
 	const { function_schema } = useParams<{ function_schema: string }>()
 	const { function_name } = useParams<{ function_name: string }>()
-	const [f, setF] = useState<any>({})
+    const [definition, setDefinition] = useState<string>("")
+    const [functionArguments, setFunctionArguments] = useState<string>("")
+    const [functionLanguage, setFunctionLanguage] = useState<string>("")
+    const [functionReturnType, setFunctionReturnType] = useState<string>("")
+    const [functionName, setFunctionName] = useState<string>("")
+    const [functionSchema, setFunctionSchema] = useState<string>("")
+
 	const supabaseDataService = new SupabaseDataService()
 	const loadFunction = async () => {
         console.log('loadFunction');
@@ -31,13 +39,39 @@ const DatabaseFunction: React.FC = () => {
 			console.error(error)
 		} else {
             console.log('f data', data![0]);
-			setF(data![0])
+            setDefinition(data![0].definition);
+            setFunctionArguments(data![0].function_arguments);
+            setFunctionLanguage(data![0].function_language);
+            setFunctionReturnType(data![0].function_return_type);
+            setFunctionName(data![0].function_name);
+            setFunctionSchema(data![0].function_schema);
 		}
 	}
 	useEffect(() => {
 		loadFunction()
 	}, [])
-	return (
+	function handleEditorChange(value: any, event: any) {
+		// here is the current value
+		console.log('handleEditorChange', value)
+		setDefinition(value)
+	}
+
+	function handleEditorDidMount(editor: any, monaco: any) {
+		console.log('onMount: the editor instance:', editor)
+		console.log('onMount: the monaco instance:', monaco)
+	}
+
+	function handleEditorWillMount(monaco: any) {
+		console.log('beforeMount: the monaco instance:', monaco)
+	}
+
+	function handleEditorValidation(markers: any) {
+		// model markers
+		console.log('handleEditorValidation', markers)
+		markers.forEach((marker: { message: any }) => console.log('onValidate:', marker.message))
+	}
+
+    return (
 		<IonPage>
 			<IonHeader>
 				<IonToolbar>
@@ -52,19 +86,54 @@ const DatabaseFunction: React.FC = () => {
 
 			<IonContent>
 				<IonGrid>
-					<IonRow>
-						<IonCol>name</IonCol>
-						<IonCol>type</IonCol>
-					</IonRow>
-                    {Object.keys(f).map((attribute, index) => {
-                        return (
-                            <IonRow key={attribute}>
-                                <IonCol>{attribute}</IonCol>
-                                <IonCol>{typeof f[attribute] !== 'object' ? f[attribute]:JSON.stringify(f[attribute])}</IonCol>
-                            </IonRow>
-                        )
-                    })}
+                    {/* definition: "CREATE OR REPLACE FUNCTION public.execute_sql(sqlcode text, statement_delimiter text)\n RETURNS json\n LANGUAGE plv8\n SECURITY DEFINER\nAS $function$\n\nconst arr = sqlcode.split(statement_delimiter);\nconst results = [];\n\n// this handles \"TypeError: Do not know how to serialize a BigInt\"\nfunction toJson(data) {\n  if (data !== undefined) {\n    return JSON.stringify(data, (_, v) => typeof v === 'bigint' ? `${v}#bigint` : v)\n        .replace(/\"(-?\\d+)#bigint\"/g, (_, a) => a);\n  }\n}\n\nfor (let i = 0; i < arr.length; i++) {\n    if (arr[i].trim() !== '') {\n        const result = plv8.execute(arr[i]);\n        results.push(toJson(result));\n    }\n}\nreturn results;\n\n$function$\n"
+                    function_arguments: "sqlcode text, statement_delimiter text"
+                    function_language: "plv8"
+                    function_name: "execute_sql"
+                    function_schema: "public"
+                    return_type: "json"                     */}
+
+                    <IonRow key="schema">
+                        <IonCol size="2">Schema</IonCol>
+                        <IonCol>{functionSchema}</IonCol>
+                    </IonRow>
+                    <IonRow key="name">
+                        <IonCol size="2">Name</IonCol>
+                        <IonCol>{functionName}</IonCol>
+                    </IonRow>
+                    <IonRow key="language">
+                        <IonCol size="2">Language</IonCol>
+                        <IonCol>{functionLanguage}</IonCol>
+                    </IonRow>
+                    <IonRow key="arguments">
+                        <IonCol size="2">Arguments</IonCol>
+                        <IonCol>{functionArguments}</IonCol>
+                    </IonRow>
+                    <IonRow key="return_type">
+                        <IonCol size="2">Return Type</IonCol>
+                        <IonCol>{functionReturnType}</IonCol>
+                    </IonRow>
 				</IonGrid>
+                <Editor
+								className='textarea'
+								height="60vh"
+								defaultLanguage='sql'
+								defaultValue={definition}
+								value={definition}
+								theme={
+									window.matchMedia('(prefers-color-scheme: dark)').matches ? 'vs-dark' : 'vs-light'
+								}
+								onChange={debounce(handleEditorChange, 750)}
+								onMount={handleEditorDidMount}
+								beforeMount={handleEditorWillMount}
+								onValidate={handleEditorValidation}
+								options={{
+									minimap: {
+										enabled: false,
+									},
+								}}
+							/>
+
 			</IonContent>
 		</IonPage>
 	)
