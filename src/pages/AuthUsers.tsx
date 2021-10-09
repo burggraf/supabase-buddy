988965ff7976af14feb34998bd8e744943ce4284
Mark Burggraf <markb@mantisbible.com>
2021-10-09 05:42:32 -0700

    user context menu: send password reset link or magic link - working

--- src/pages/AuthUsers.tsx
+++ src/pages/AuthUsers.tsx
@@ -1,15 +1,22 @@
-import { IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonLabel, IonMenuButton, IonPage, IonRow, IonTitle, IonToolbar, useIonViewDidEnter } from '@ionic/react';
+import { IonButton, IonButtons, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonListHeader, IonMenuButton, IonPage, IonPopover, IonRow, IonTitle, IonToolbar, useIonToast, useIonViewDidEnter } from '@ionic/react';
 import { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router';
 import { SupabaseDataService } from '../services/supabase.data.service';
 import './AuthUsers.css';
 import Moment from 'moment';
-import { ellipsisHorizontal } from 'ionicons/icons';
+import { ellipsisHorizontal, mail } from 'ionicons/icons';
+import { SupabaseAuthService } from '../services/supabase.auth.service';
 
 const AuthUsers: React.FC = () => {
 	const supabaseDataService = new SupabaseDataService();
+  const supabaseAuthService = new SupabaseAuthService();
     const history = useHistory();
     const [users, setUsers] = useState<any[]>([])
+    const [showPopover, setShowPopover] = useState<{open: boolean, event: Event | undefined, user: any}>({
+      open: false,
+      event: undefined,
+      user: undefined
+    });
 	const loadUsers = async () => {
 		const { data, error } = await supabaseDataService.getUsers()
 		if (error) {
@@ -24,13 +31,38 @@ const AuthUsers: React.FC = () => {
 		console.log('useIonViewDidEnter...')
 		loadUsers()
 	})
-  const userContextMenu = (user: any) => {
-    console.log('not ready yet...user:', user);
-  }
 	useEffect(() => {
 		//loadUsers()
 	}, [])
+	const [presentToast, dismissToast] = useIonToast();
+    const toast = (message: string, color: string = 'danger') => {
+        presentToast({
+            color: color,
+            message: message,
+            cssClass: 'toast',
+            buttons: [{ icon: 'close', handler: () => dismissToast() }],
+            duration: 6000,
+            //onDidDismiss: () => console.log('dismissed'),
+            //onWillDismiss: () => console.log('will dismiss'),
+          })
+    }
   const { name } = useParams<{ name: string; }>();
+  const sendPasswordResetEmail = async (email: any) => {
+    const { data, error} = await supabaseAuthService.resetPassword(email);
+    if (!error) {
+      toast('Password reset email sent.', 'success')
+    } else {
+      toast('Error: ' + error?.message, 'danger')
+    }    
+  }
+  const sendMagicLink = async (email: any) => {
+    const { user, session, error} = await supabaseAuthService.sendMagicLink(email);
+    if (!error) {
+      toast('Magic link email sent.', 'success')
+    } else {
+      toast('Error: ' + error?.message, 'danger')
+    }
+  }
 
   return (
     <IonPage>
@@ -75,8 +107,8 @@ const AuthUsers: React.FC = () => {
                         <IonLabel>{Moment(user.last_sign_in_at).format('YYYY-MM-DD hh:mmA')}</IonLabel>
                     </IonCol>
                     <IonCol className="ion-text-center">
-                      <IonButton fill='outline' color="medium" onClick={(e) => {e.stopPropagation();userContextMenu(user)}}>
-	        							<IonIcon size="small" icon={ellipsisHorizontal}></IonIcon>
+                      <IonButton fill='outline' color="medium" onClick={(e) => {e.stopPropagation();setShowPopover({open: true, event: e.nativeEvent, user})}}>
+	        							<IonIcon size="small" icon={ellipsisHorizontal}></IonIcon>                        
 					        		</IonButton>
                     </IonCol>
                         {/* <IonCol>
@@ -85,7 +117,28 @@ const AuthUsers: React.FC = () => {
                 </IonRow>                        
             ))}         
         </IonGrid>
-
+        <IonPopover
+          isOpen={showPopover.open}
+          event={showPopover.event}
+          onDidDismiss={e => setShowPopover({open: false, event: undefined, user: undefined})}>
+          <IonList>
+            <IonListHeader>
+              <IonLabel className="ion-text-center">{showPopover.user?.email}</IonLabel>
+            </IonListHeader>
+            <IonItem>
+              <IonButton fill="clear" onClick={()=>{sendPasswordResetEmail(showPopover.user?.email)}} color="dark">
+                <IonIcon slot="start" size="small" icon={mail}></IonIcon> 
+                Send Password Reset Email
+              </IonButton>
+            </IonItem>
+            <IonItem>
+              <IonButton fill="clear" onClick={()=>{sendMagicLink(showPopover.user?.email)}} color="dark">
+                <IonIcon slot="start" size="small" icon={mail}></IonIcon> 
+                Send Magic Link
+              </IonButton>
+            </IonItem>
+          </IonList>
+        </IonPopover>
       </IonContent>
     </IonPage>
   );
