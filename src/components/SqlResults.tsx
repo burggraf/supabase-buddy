788ccc9c394708fed31d578db049275254b30892
Mark Burggraf <markb@mantisbible.com>
2022-01-17 11:57:35 -0800

    move SqlResults to use TableGrid (simplify)

--- src/components/SqlResults.tsx
+++ src/components/SqlResults.tsx
@@ -1,24 +1,23 @@
 import { IonLabel } from '@ionic/react'
+import { TableGrid } from 'ionic-react-tablegrid'
 import { useState } from 'react'
 import UtilsService from '../services/utils.service'
 import DisplayDetail from './DisplayDetail'
 import './SqlResults.css'
 
-
-
-const utilsService = UtilsService.getInstance();
+const utilsService = UtilsService.getInstance()
 interface ContainerProps {
 	results: any[]
 }
 
 const SqlResults: React.FC<ContainerProps> = ({ results }) => {
 	const [record, setRecord] = useState({})
-    const [resultSet, setResultSet] = useState(0)
+	const [resultSet, setResultSet] = useState(0)
 	const [detailTrigger, setDetailTrigger] = useState({ action: '' })
 	const [currentIndex, setCurrentIndex] = useState(1)
 
 	const outputArray = []
-    const resultSetArray: any[] = []
+	const resultSetArray: any[] = []
 	for (let i = 0; i < results.length; i++) {
 		const result = results[i]
 		let resultJson
@@ -29,57 +28,37 @@ const SqlResults: React.FC<ContainerProps> = ({ results }) => {
 			resultJson = result
 		}
 		// test if resultJson is an array
-		if (resultJson && Array.isArray(resultJson)) {  
-            resultSetArray.push(resultJson)
-            if (resultJson.length > 0) {
+		if (resultJson && Array.isArray(resultJson)) {
+			resultSetArray.push(resultJson)
+			if (resultJson.length > 0) {
 				// get keys and values of first element
 				const keys = Object.keys(resultJson[0])
-				const { gridWidth, columnWidths } = utilsService.getGridWidths(resultJson);
+				const resultRows: any = [];
+				resultJson.map((row, index) => {
+					keys.map((key, index) => {
+						if (typeof row[key] === 'object') {
+							row[key] = JSON.stringify(row[key]);
+						}
+					})
+					resultRows.push(row);
+				});
+				const { gridWidth, columnWidths } = utilsService.getGridWidths(resultJson)
 				outputArray.push(
-					<div key={utilsService.randomKey()}>
-						<div key={utilsService.randomKey()} className='resultHeader'>
-							<strong>Result #{i + 1}</strong>
+					<>
+						<div key={utilsService.randomKey()}>
+							<div key={utilsService.randomKey()} className='resultHeader'>
+								<strong>Result #{i + 1}</strong>
+							</div>
 						</div>
-						<table style={{'width': gridWidth + 'px'}} key={utilsService.randomKey()}>
-							<tbody>
-							<tr key={utilsService.randomKey()}>
-								{keys.map((key, index) => (
-									<td style={{'width': columnWidths[index] + 'px'}} className='breakItUp' key={utilsService.randomKey()}>
-										<strong>{key}</strong>
-									</td>
-								))}
-							</tr>
-							{resultJson.map((row, index) => (
-								<tr
-									key={utilsService.randomKey()}
-									onClick={() => {
+						<TableGrid rows={resultRows} rowClick={(row, index) => {
 										console.log('onclick fired')
                                         setResultSet(i)
                                         setCurrentIndex(index+1)
 										setRecord(row)
 										setDetailTrigger({ action: 'open' })
-									}}>
-									{keys.map((key, index) => {
-										// if (!Array.isArray(row[key])) {
-										if (typeof row[key] !== 'object') {
-											return (
-												<td style={{'width': columnWidths[index] + 'px'}} className='breakItUp boxed' key={utilsService.randomKey()}>
-													{row[key]}
-												</td>
-											)
-										} else {
-											return (
-												<td style={{'width': columnWidths[index] + 'px'}} className='breakItUp boxed' key={utilsService.randomKey()}>
-													{JSON.stringify(row[key])}
-												</td>
-											)
-										}
-									})}
-								</tr>
-							))}
-							</tbody>
-						</table>
-					</div>
+							
+						}} />
+					</>
 				)
 				// resultJson[0]
 			}
@@ -105,25 +84,28 @@ const SqlResults: React.FC<ContainerProps> = ({ results }) => {
 			)
 		}
 	}
-	return <>{outputArray}
-			<DisplayDetail                 
-				rec={record} 
-				trigger={detailTrigger}  
-				current={currentIndex} 
+	return (
+		<>
+			{outputArray}
+			<DisplayDetail
+				rec={record}
+				trigger={detailTrigger}
+				current={currentIndex}
 				total={resultSetArray[resultSet] ? resultSetArray[resultSet].length : null}
-				onBack={()=>{		
-					const newIndex = currentIndex - 1;
-					setCurrentIndex(newIndex);
-					setRecord(resultSetArray[resultSet][newIndex-1]);
+				onBack={() => {
+					const newIndex = currentIndex - 1
+					setCurrentIndex(newIndex)
+					setRecord(resultSetArray[resultSet][newIndex - 1])
 				}}
-				onForward={()=>{
-					const newIndex = currentIndex + 1;
-					setCurrentIndex(newIndex);
-					setRecord(resultSetArray[resultSet][newIndex-1]);					
+				onForward={() => {
+					const newIndex = currentIndex + 1
+					setCurrentIndex(newIndex)
+					setRecord(resultSetArray[resultSet][newIndex - 1])
 				}}
-                title={`Result #${resultSet+1} Details`}
+				title={`Result #${resultSet + 1} Details`}
 			/>
-    </>
+		</>
+	)
 }
 
 export default SqlResults
